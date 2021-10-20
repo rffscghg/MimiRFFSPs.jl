241ddf51376faaa05f862b053e17c5e0341478c5
David Anthoff <anthoff@berkeley.edu>
2021-10-20 09:48:46 -0700

    Use DataDeps.jl for file download

--- src/MimiRFFSPs.jl
+++ src/MimiRFFSPs.jl
@@ -1,8 +1,22 @@
 module MimiRFFSPs
 
+using DataDeps
+
 global g_datasets = Dict{Symbol,Any}()
 
 include("components/SPs.jl")
 include("components/RegionAggregatorSum.jl")
+
+function __init__()
+    register(DataDep(
+        "rffsps",
+        "RFF SPs prerelease version.",
+        "https://rffsps.s3.us-west-1.amazonaws.com/RFFSPs_large_datafiles.7z",
+        post_fetch_method=unpack
+    ))
+
+    # Now trigger a download
+    datadep"rffsps"
+end
 
 end # module
